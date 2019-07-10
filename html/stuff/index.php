ee5bf0f1a87ecbc45506b98908ce11987cb1262d
Bryn Dyment <bryndyment@gmail.com>
2019-07-10 20:42:01 +0000

    automated commit

--- html/stuff/index.php
+++ html/stuff/index.php
@@ -1,4 +1,40 @@
-<!-- AIzaSyCJMkvWPpwBp4nY2A_SkavUzUU0zaj-s6I -->
+<!DOCTYPE html>
+
 <?php
 	echo "hello";
 ?>
+
+<html>
+  <head>
+    <title>Come Facciamo 2019</title>
+    <meta name="viewport" content="initial-scale=1.0">
+    <meta charset="utf-8">
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAaSsrqvX1yY-FVcIsfXTqR-MDFgICC7io&callback=initMap"
+    async defer></script>
+  </body>
+</html>

876383ba7e1147f4efdb2c44368486f3d6ea1011
Barnaby Gunning <barnabygunning@me.com>
2019-09-28 10:08:01 +0100

    REVISION 10:06 28/09/19

--- html/stuff/index.php
+++ html/stuff/index.php
@@ -4,7 +4,7 @@
 	?>
 <html>
   <head>
-    <title>REVISION 10:08 28/09/19</title>
+    <title>REVISION 10:05 28/09/19</title>
     <meta name="viewport" content="initial-scale=1.0">
     <meta charset="utf-8">
     <style>
@@ -27,18 +27,21 @@
       var map;
       var school = ['Convitto Nazionale MUSP',42.3661079,13.3584066];
       var block = Array('1', 'ClickDay 01', '42.351', '13.3971', '42.352131,13.39682;42.351372,13.39844;42.350719,13.39997;42.3489,13.39908;42.3493,13.39793;42.35059,13.39566;42.350971,13.395944;42.351234,13.39529;42.351311,13.39529;42.352032,13.3967;42.352131,13.39682', '0', 'Zones', 'v');
-
+	  console.log(school[0],school[1]);
+	  
       function initMap() {
 	     var latitude = parseFloat(block[2]);
 		 var longitude = parseFloat(block[3]);
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat:latitude, lng:longitude},
           zoom: 16,
-         
+          styles: [
+	          { featureType: 'poi', stylers: [{visibility: 'off'}] }
+	          ]
         });
-        var pos = {lat: school[1], lng: school[2]};
+        
         var marker = new google.maps.Marker({
-	         position: pos,
+	         position: {lat: school[1], lng: school[2]},
 			 title: school[0]
         })
         

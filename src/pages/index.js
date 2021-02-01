5e4f86ff3971505f332735c65adc02ea8ae7842c
Bryn Dyment <bryndyment@gmail.com>
2021-02-01 19:47:54 +0900

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,33 @@
 import { Helmet } from "react-helmet";
 import React from "react";
 
-const heroes = ["170_0013", "200_0016", "202_0073", "211_6514"];
+// exported components
 
-const backgroundImage = `url(/${heroes[Math.floor(Math.random() * heroes.length)]}.jpg)`;
+const HomePage = () => {
+  const heroes = ['170_0013', '200_0016', '202_0073', '211_6514']
 
-const HomePage = () => (
-  <>
-    <Helmet>
-      <title>Barnaby Gunning Studio</title>
-    </Helmet>
+  const backgroundImage = `url(/${heroes[Math.floor(Math.random() * heroes.length)]}.jpg)`
 
-    <div id="bkg" style={{ backgroundImage }}>
-      <div id="hero"></div>
-      <div id="logo"></div>
-      <div id="title">Barnaby Gunning Studio</div>
-      <div id="contact">
-        63 Loudoun Road,
-        <br />
-        London NW8 0DQ.
-        <br />
-        +44 20 39 35 79 88
+  return (
+    <>
+      <Helmet>
+        <title>Barnaby Gunning Studio</title>
+      </Helmet>
+
+      <div id="bkg" style={{ backgroundImage }}>
+        <div id="hero"></div>
+        <div id="logo"></div>
+        <div id="title">Barnaby Gunning Studio</div>
+        <div id="contact">
+          63 Loudoun Road,
+          <br />
+          London NW8 0DQ.
+          <br />
+          +44 20 39 35 79 88
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  )
+}
 
-export default HomePage;
+export default HomePage
